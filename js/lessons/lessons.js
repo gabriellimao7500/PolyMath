e4c9e79e4ca6740e304d130749ece56890668766
gabriellimao7500 <gabriellimao7500@gmail.com>
2023-11-29 19:09:46 -0300

    update

--- js/lessons/lessons.js
+++ js/lessons/lessons.js
@@ -8,15 +8,142 @@ const aula7 = document.querySelector(".A7")
 const aula8 = document.querySelector(".A8")
 const aula9 = document.querySelector(".A9")
 
+const icon1 = document.querySelector(".icon1")
+const icon2 = document.querySelector(".icon2")
+const icon3 = document.querySelector(".icon3")
+const icon4 = document.querySelector(".icon4")
+const icon5 = document.querySelector(".icon5")
+const icon6 = document.querySelector(".icon6")
+const icon7 = document.querySelector(".icon7")
+const icon8 = document.querySelector(".icon8")
+const icon9 = document.querySelector(".icon9")
+
 var aulaAtual = localStorage.getItem("numAulaCompl");
-if(isNaN == aulaAtual || aulaAtual == null){
+if(isNaN == aulaAtual || aulaAtual == null || aulaAtual == 0){
     localStorage.setItem("numAulaCompl",1);
     aulaAtual = localStorage.getItem("numAulaCompl");
 }
-
 console.log(aulaAtual)
+if(aulaAtual == 2){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("learn-img")
+    icon2.classList.remove("lock-img")
+}
+if(aulaAtual == 3){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.remove("lock-img")
+    icon3.classList.add("learn-img")
+}
+if(aulaAtual == 4){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("learn-img")
+}
+if(aulaAtual == 5){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
 
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("learn-img")
+}
+if(aulaAtual == 6){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("complete-img")
 
+    icon6.classList.remove("lock-img")
+    icon6.classList.add("learn-img")
+}
+if(aulaAtual == 7){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("complete-img")
+    icon6.classList.remove("lock-img")
+    icon6.classList.add("complete-img")
+
+    icon7.classList.remove("lock-img")
+    icon7.classList.add("learn-img")
+}
+if(aulaAtual == 8){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("complete-img")
+    icon6.classList.remove("lock-img")
+    icon6.classList.add("complete-img")
+    icon7.classList.remove("lock-img")
+    icon7.classList.add("complete-img")
+
+    icon8.classList.remove("lock-img")
+    icon8.classList.add("learn-img")
+}
+if(aulaAtual == 9){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("complete-img")
+    icon6.classList.remove("lock-img")
+    icon6.classList.add("complete-img")
+    icon7.classList.remove("lock-img")
+    icon7.classList.add("complete-img")
+    icon8.classList.remove("lock-img")
+    icon8.classList.add("complete-img")
+
+    icon9.classList.remove("lock-img")
+    icon9.classList.add("learn-img")
+}
+if(aulaAtual == 10){
+    icon1.classList.add("complete-img")
+    icon2.classList.add("complete-img")
+    icon2.classList.remove("lock-img")
+    icon3.classList.add("complete-img")
+    icon3.classList.remove("lock-img")
+    icon4.classList.remove("lock-img")
+    icon4.classList.add("complete-img")
+    icon5.classList.remove("lock-img")
+    icon5.classList.add("complete-img")
+    icon6.classList.remove("lock-img")
+    icon6.classList.add("complete-img")
+    icon7.classList.remove("lock-img")
+    icon7.classList.add("complete-img")
+    icon8.classList.remove("lock-img")
+    icon8.classList.add("complete-img")
+    icon9.classList.remove("lock-img")
+    icon9.classList.add("complete-img")
+}
 aula1.addEventListener("click", () => {
     if(aulaAtual > 0){
         localStorage.setItem("aulaAtual",1);
