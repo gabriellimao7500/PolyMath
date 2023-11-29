802fce6373acfa5824b346135fff95b1666ae232
gabriellimao7500 <gabriellimao7500@gmail.com>
2023-11-29 16:10:36 -0300

    update

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,21 +1,44 @@
 const statistics = document.getElementById('statistics')
+const learn = document.querySelector('.learn')
+const ranking = document.getElementById('ranking')
+const perfil = document.getElementById('perfil')
 const exit = document.querySelector('.off')
-const learn = document.getElementById('learn')
 
-statistics.addEventListener("click", () => {
-    setTimeout(ir,400)
+
+console.log(learn)
+
+ranking.addEventListener("click", () => {
+    setTimeout(irranking,400)
     exit.classList.add('off-locate')
-    function ir(){
-        window.location.href = "html/statistics.html"
+    function irranking(){
+        window.location.href = "ranking.html"
     }
 })
 
-console.log(exit)
+
+perfil.addEventListener("click", () => {
+    setTimeout(irperfil,400)
+    exit.classList.add('off-locate')
+    function irperfil(){
+        window.location.href = "perfil.html"
+    }
+})
+
+statistics.addEventListener("click", () => {
+    setTimeout(irhome,400)
+    exit.classList.add('off-locate')
+    function irhome(){
+        window.location.href = "statistics.html"
+    }
+})
 
 learn.addEventListener("click", () => {
     setTimeout(ir,400)
     exit.classList.add('off-locate')
     function ir(){
-        window.location.href = "../index.html"
+        window.location.href = "main.html"
     }
 })
+
+
+
